58cd397194426e32568fd151ca87b92129861dc5
jolson615 <jolson615@gmail.com>
2017-09-18 14:25:57 -0400

    stretch challenges

--- cards.rb
+++ cards.rb
@@ -1,2 +1,17 @@
 puts "What is your name?"
 name = gets
+
+
+
+
+
+
+
+
+
+
+
+# Additional challenges:
+# Tab in the phone number and position elements.
+# Try to split the motto into multiple lines if it's longer than the bars at the top and bottom of the card.
+# Try to make the name centered. There are various ways to do this, and none of them are easy - most involve using the .length method somewhere.
